0e116a6f21710364f2648ba0fd16418f4c52aac5
Terrance-exe <67249951+Terrance-exe@users.noreply.github.com>
2022-09-11 17:44:39 +1000

    global reference

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import BaseLogo from './components/BaseLogo.vue'
+import QRreader from './components/QRreader.vue'
 import './assets/style.css'
 
 const app = createApp(App)
