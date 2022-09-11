ca8d423a063aae1a7bef4e0548f4e8301b1408e7
Terrance-exe <67249951+Terrance-exe@users.noreply.github.com>
2022-09-11 17:47:53 +1000

    Update QRreader.vue

--- frontend/src/components/QRreader.vue
+++ frontend/src/components/QRreader.vue
@@ -2,23 +2,4 @@
   <StreamBarcodeReader
     @decode="onDecode"
     @loaded="onLoaded"
-  ></StreamBarcodeReader>
-</template>
-
-<script>
-import { StreamBarcodeReader } from "vue-barcode-reader";
-
-export default {
-  components: {
-    StreamBarcodeReader,
-  },
-  methods: {
-    onDecode(text) {
-      console.log(`Decode text from QR code is ${text}`);
-    },
-    onLoaded() {
-      console.log(`Ready to start scanning barcodes`);
-    },
-  },
-};
-</script>
+  ></StreamBarcodeRea
